8bf10960b1e8abe1a175cb3443b85e7ef5c5d7d1
Jimmy Zuber <jzuber@princeton.edu>
2015-04-14 15:43:42 -0400

    progress

--- gather.py
+++ gather.py
@@ -3,7 +3,10 @@
 # This script gathers information about the movies
 # based on some criteria.
 #
+from collections import OrderedDict
 from movie import Episode, Movie, MovieType
+from sklearn.feature_extraction.text import TfidfVectorizer
+import math
 import re
 import sys
 
@@ -19,10 +22,14 @@ import sys
 # episodes have additional formatting:
 # "xxxxxx" (year) {episode title (#season_number.episode_index_number)} year
 # NOTE: since the current data doesn't have miniseries, they aren't supported
-def parse_line(line):
+def parse_movie_line(line):
     # remove starting and trailing whitespace, check if empty line
     line = line.strip()
     if line == "":
+        return None
+
+    # don't consider suspended movies
+    if "{{SUSPENDED}}" in line:
         return None
 
     # miniseries not supported
@@ -36,52 +43,90 @@ def parse_line(line):
     # for tv-series that span multiple years
     if "-" in year:
         year, year_end = year.split("-")
-    try:
+    if year != "????":
         year = int(year)
-    except ValueError:
-        raise ValueError('Year "{}" is not an integer'.format(year))
 
     # get the title, title is before (year) in line
-    split = line.split('({})'.format(year))
-    if len(split) != 2:
-        raise ValueError('Line has ({}) in it {} times'.format(year, len(split) - 1))
-    title = split[0].strip()
-    after = split[1].strip()
+    regex = re.compile(r'\(\d\d\d\d(/([IVXLC]+))?\)|\(\?\?\?\?(/([IVXLC]+))?\)')
+    index = regex.search(line).end()
+    title = line[:index]
+    after = line[index:]
 
     # default movie type
     movie_type = MovieType.theater
-    # if the movie is a tv-series (episode
-    if title[0] == "\"" and title[-1] == "\"":
+    # if the movie is a tv-series
+    if title[0] == "\"":
         movie_type = MovieType.tv_series
-        title = title[1:-1]
     elif "(TV)" in after:
         movie_type = MovieType.tv
     elif "(V)" in after:
         movie_type = MovieType.video
-
-    # if its an episode
-    if movie_type == MovieType.tv_series and "{" in after and "}" in after:
-        _, after = after.split("{")
-        after, _ = after.split("}")
-        index = re.search(r'\(#\d+\.\d+\)', after).lastindex
-        episode_title = after[:index]
-        season_episode = after[index:]
-        print(episode_title, season_episode)
-        print(after)
-        season, episode_index = season_episode[2:-1].split(".")
-        try:
-            season = int(season)
-            episode_index = int(episode_index)
-        except ValueError:
-            raise ValueError('season "{}" and episode index "{}" not ints'.format(season, episode_index))
-        return Episode(episode_title, title, season, episode_index, year)
+    elif "(VG)" in after:
+        movie_type = MovieType.video_game
 
     return Movie(title, movie_type, year, year_end)
 
 def main():
-    file_name = "data/movies.list" if len(sys.argv) < 2 else sys.argv[1]
-    with open(file_name, encoding="latin1") as f:
-        for l in f:
-            parse_line(l)
+    encoding = "latin1"
+    # regex to find end of movie title
+    regex = re.compile(r'\(\d\d\d\d(/([IVXLC]+))?\)|\(\?\?\?\?(/([IVXLC]+))?\)')
+    # indices for movies and keywords
+    movie_indices = {}
+    # contian
+    movies = OrderedDict()
+
+    print("Reading in movies...")
+    with open("data/movies.list") as f:
+        for line in f:
+            movie = parse_movie_line(line)
+            # invalid movies
+            if not movie:
+                continue
+            # movies of incorrect type
+            if movie.movie_type != MovieType.theater:
+                print("Only considering regular movies.")
+                continue
+            movies[movie.name] = []
+            # set index of movie
+            if not movie.name in movie_indices:
+                movie_indices[movie.name] = len(movie_indices)
+
+    print("Reading keywords...")
+    with open("data/keywords.list") as f:
+        for line in f:
+            index = regex.search(line).end()
+            name = line[:index].strip()
+            # only consider matching movies
+            if not name in movies:
+                continue
+            keyword = line[index:].strip()
+            # add keyword to movie
+            keywords = movies[name]
+            keywords.append(keyword)
+
+    print("Eliminating movies without keywords...")
+    for name, v in movies.items():
+        if not v:
+            del movies[name]
+        else:
+            movies[name] = " ".join(v)
+
+    # number of movies being clustered
+    N = len(movies)
+    print("Total movies: {}".format(N))
+
+    print("Computing tf-idf matrix...")
+    def tokenizer(string):
+        return string.split()
+    vectorizer = TfidfVectorizer(encoding=encoding, tokenizer=tokenizer)
+    X = vectorizer.fit_transform(movies.items())
+
+    print("Done!")
+
+
+
+
+
+
 
 main()
--- movie.py
+++ movie.py
@@ -1,16 +1,34 @@
 # movie.py
 # Author: Jimmy Zuber
 # This file contains the movie class
-from enum import Enum
-
-class MovieType(Enum):
+class MovieType():
   theater        = 0 # your ordinary movie, released in theaters
   tv_series      = 1 # a tv series
   tv_mini_series = 2 # a tv mini-series
   tv             = 3 # a movie released for tv
   video          = 4 # a movie released straight to video
+  video_game     = 5 # a video game movie
 
 class Movie():
+  def __repr__(self):
+    s = "Movie- Name: {}, Year: {}, ".format(self.name, self.year)
+    if hasattr(self, "year_end"):
+        s += "Year End: {}, ".format(self.year_end)
+
+    if self.movie_type == MovieType.theater:
+        type_string = "Theater"
+    elif self.movie_type == MovieType.tv_series:
+        type_string = "TV Series"
+    elif self.movie_type == MovieType.tv_mini_series:
+        type_string = "TV Mini Series"
+    elif self.movie_type == MovieType.tv:
+        type_string = "TV"
+    elif self.movie_type == MovieType.video:
+        type_string = "Video"
+
+    s += "Type: {}".format(type_string)
+    return s
+
   def __init__ (self, name, movie_type, year, year_end = None):
     self.name       = name
     self.movie_type = movie_type
@@ -22,9 +40,13 @@ class Movie():
 
 
 class Episode():
-  def __init__ (self, name, movie_title, season, episode_index, year):
+  def __repr__(self):
+      template = "Episode- Name: {}, Series Title: {}, Season: {}, Episode: {}, Year: {}"
+      return template.format(self.name, self.series_title, self.season, self.episode_index, self.year)
+  def __init__ (self, name, series_title, season, episode_index, year):
     self.name          = name
-    self.movie_title   = movie_title
+    self.series_title  = series_title
     self.season        = season
     self.episode_index = episode_index
     self.year          = year
+
